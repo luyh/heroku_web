8ccd28887a5d66bd223f011e3bb0afba565170e5
luyh <yanhonglu5@icloud.com>
2019-03-23 22:27:52 +0800

    add fgi

--- clock.py
+++ clock.py
@@ -1,10 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from mypackage import fgi
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=10)
 def timed_job():
     print('This job is run every 10 seconds.')
+    data = fgi.get_fear_greed_index()[::-1]
+    print( data )
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
